308cde3bb8aec6ea75fa2fe53ecc1e6335a3cf16
Strauss66 <johang@byui.edu>
2025-01-30 22:25:07 -0700

    backend working, made changes in server.js and some on the .env

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const dotenv = require('dotenv');
-// Load environment variables
-dotenv.config();
+const path = require('path');
+
+// Force load .env file
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('DB_NAME:', process.env.DB_NAME);
+console.log('DB_HOST:', process.env.DB_HOST);
+
 const cors = require('cors');
 const sequelize = require('./config/db');
 const Student = require('./models/Student');
